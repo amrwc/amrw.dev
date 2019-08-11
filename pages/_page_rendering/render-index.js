75b8b43869f7015fbbfc09656b030dbd66a26c35
amrwc <42574496+amrwc@users.noreply.github.com>
2019-08-11 19:01:42 +0100

    Update render-index script, update page-rendering notes

--- pages/_page_rendering/render-index.js
+++ pages/_page_rendering/render-index.js
@@ -9,11 +9,16 @@
 
 const mume = require('@shd101wyy/mume');
 const fs = require('fs');
+const { exec } = require('child_process');
+
+PATH_README_MD = '../../README.md';
+PATH_README_HTML = '../../README.html';
+PATH_INDEX_HTML = '../../index.html';
 
 (async function() {
   await mume.init();
   const engine = new mume.MarkdownEngine({
-    filePath: `../../README.md`,
+    filePath: PATH_README_MD,
     config: {
       previewTheme: 'github-light.css',
       codeBlockTheme: 'github.css',
@@ -22,8 +27,26 @@ const fs = require('fs');
     }
   });
   await engine.htmlExport({ offline: false, runAllCodeChunks: true });
-  await fs.rename('../../README.html', '../../index.html', err => {
-    if (err) console.error(err);
-  });
+  await fs.renameSync(PATH_README_HTML, PATH_INDEX_HTML, err =>
+    handleError(err)
+  );
+
+  // Replace the `.md` href extensions with `.html`
+  exec(
+    `perl -i -pe "s/.md\\">/.html\\">/g" ${PATH_INDEX_HTML}`,
+    (err, stdout, stderr) => {
+      handleError(err);
+      console.log(stdout);
+      console.error(stderr);
+    }
+  );
+
   return process.exit();
 })();
+
+function handleError(err) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
