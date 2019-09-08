442cd22793c436a9ebf2077552cd14334f6d90ec
amrwc <42574496+amrwc@users.noreply.github.com>
2019-09-08 13:36:20 +0200

    Start writing a logic for detecting/distinguishing symlinks and files
    The current solution works, but creates a dated backup of a sym-
    link rather than skipping the copying block and jumping to un-
    link. What's causing it is the unreliable check for whether the
    file exists ('fs.existsSync'), since it gives unpredictable re-
    sults when ran on symlinks.

--- scripts/dotfiles/link_utils.js
+++ scripts/dotfiles/link_utils.js
@@ -19,36 +19,52 @@ exports.getCurrentDateTime = function() {
  * @param {string} srcDirPath
  * @param {string} destDirPath
  */
-exports.linkDotfile = function(srcFileName, destFileName, srcDirPath, destDirPath) {
+exports.linkDotfile = async function(srcFileName, destFileName, srcDirPath, destDirPath) {
   const path = require('path');
   const fs = require('fs');
 
-  const sourceFilePath = path.resolve(`${srcDirPath}/${srcFileName}`);
-  const destinationFilePath = path.resolve(`${destDirPath}/${destFileName}`);
+  const srcFilePath = path.resolve(`${srcDirPath}/${srcFileName}`);
+  const destFilePath = path.resolve(`${destDirPath}/${destFileName}`);
 
-  const errorCallback = err => {
+  const errorCallbackGeneric = err => {
     if (err) {
       console.error(err);
       process.exit(1);
     }
   };
 
-  // If the file exists...
-  if (fs.existsSync(destinationFilePath)) {
-    // ...make its dated copy before replacing it with a symlink.
-    const backupFilePath =
-      destDirPath +
-      '/' +
-      path.basename(destinationFilePath, path.extname(destinationFilePath)) +
-      '_' +
-      exports.getCurrentDateTime() +
-      path.extname(destinationFilePath);
+  // NOTE: 'fs.existsSync' gives unexpected results when supplied a symlink.
+  //       Find more reliable way of detecting whether a real file exists,
+  //       or whether it's a symlink.
+  const existsDestFile = fs.existsSync(destFilePath);
+  // const existsDestFile = (await fs.open(destFilePath, 'r', (err, fd) => {
+  //   errorCallbackGeneric(err);
+  // }))
+  //   ? true
+  //   : false;
 
-    fs.copyFile(destinationFilePath, backupFilePath, errorCallback);
-    fs.unlink(destinationFilePath, errorCallback);
+  let existsDestSymlink = false;
+  await fs.lstat(destFilePath, (err, stats) => {
+    if (!err && stats) existsDestSymlink = true;
+  });
+
+  // If the file or a symlink with the same path exists...
+  if (existsDestFile || existsDestSymlink) {
+    if (existsDestFile && !existsDestSymlink) {
+      // ...make a dated copy of the file before replacing it with a symlink.
+      const backupFilePath =
+        destDirPath +
+        '/' +
+        path.basename(destFilePath, path.extname(destFilePath)) +
+        '_' +
+        exports.getCurrentDateTime() +
+        path.extname(destFilePath);
+
+      await fs.copyFile(destFilePath, backupFilePath, errorCallbackGeneric);
+    }
+
+    await fs.unlink(destFilePath, errorCallbackGeneric);
   }
-  // TODO: Create an else-if statement to check whether a symlink already exists
-  //	     and simply use unlink on it.
 
-  fs.symlink(sourceFilePath, destinationFilePath, errorCallback);
+  await fs.symlink(srcFilePath, destFilePath, errorCallbackGeneric);
 };
