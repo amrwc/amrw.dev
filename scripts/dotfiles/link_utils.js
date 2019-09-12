98ea9f57312ee641c9ca87605bff586cbb4d27e6
amrwc <42574496+amrwc@users.noreply.github.com>
2019-09-12 10:55:23 +0200

    Display more information on runtime, refactor small pieces of the utils
    - add more information to be displayed on runtime,
    - rename the text utilities function and it's keys.

--- scripts/dotfiles/link_utils.js
+++ scripts/dotfiles/link_utils.js
@@ -1,39 +1,39 @@
 /**
- * Returns a colour escape sequence that can be used to create coloured console outputs.
+ * Returns an escape sequence that can be used to format console outputs.
  *
  * @source https://stackoverflow.com/a/41407246/10620237
- * @param {string} colour
+ * @param {string} util
  */
-exports.getColour = function(colour) {
-  const colours = {
-    Reset: '\x1b[0m',
-    Bright: '\x1b[1m',
-    Dim: '\x1b[2m',
-    Underscore: '\x1b[4m',
-    Blink: '\x1b[5m',
-    Reverse: '\x1b[7m',
-    Hidden: '\x1b[8m',
+exports.txtUtils = function(util) {
+  const utils = {
+    reset: '\x1b[0m',
+    bright: '\x1b[1m',
+    dim: '\x1b[2m',
+    underscore: '\x1b[4m',
+    blink: '\x1b[5m',
+    reverse: '\x1b[7m',
+    hidden: '\x1b[8m',
 
-    FgBlack: '\x1b[30m',
-    FgRed: '\x1b[31m',
-    FgGreen: '\x1b[32m',
-    FgYellow: '\x1b[33m',
-    FgBlue: '\x1b[34m',
-    FgMagenta: '\x1b[35m',
-    FgCyan: '\x1b[36m',
-    FgWhite: '\x1b[37m',
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
 
-    BgBlack: '\x1b[40m',
-    BgRed: '\x1b[41m',
-    BgGreen: '\x1b[42m',
-    BgYellow: '\x1b[43m',
-    BgBlue: '\x1b[44m',
-    BgMagenta: '\x1b[45m',
-    BgCyan: '\x1b[46m',
-    BgWhite: '\x1b[47m',
+    bgBlack: '\x1b[40m',
+    bgRed: '\x1b[41m',
+    bgGreen: '\x1b[42m',
+    bgYellow: '\x1b[43m',
+    bgBlue: '\x1b[44m',
+    bgMagenta: '\x1b[45m',
+    bgCyan: '\x1b[46m',
+    bgWhite: '\x1b[47m',
   };
 
-  return colours[colour];
+  return utils[util];
 };
 
 /**
@@ -72,7 +72,7 @@ exports.linkDotfile = function(srcFileName, destFileName, srcDirPath, destDirPat
       ? true
       : false;
 
-  // If the destination exists...
+  // If the full destination path exists...
   if (existsDestFile) {
     // ...test whether it's a symlink, since the '-f' flag in 'test' is true for symlinks too...
     const existsDestSymlink =
@@ -90,17 +90,27 @@ exports.linkDotfile = function(srcFileName, destFileName, srcDirPath, destDirPat
         exports.getCurrentDateTime() +
         path.extname(destFilePath);
       fs.copyFileSync(destFilePath, backupFilePath);
-      console.info(`${exports.getColour('FgYellow')}Created a dated backup of ${destFileName}`);
+      console.info(
+        `${exports.txtUtils('yellow')}Created a dated backup of ${destFileName}:\n `,
+        `${exports.txtUtils('reset')}${backupFilePath}`
+      );
     }
     // ...then remove the existing file/symlink.
     fs.unlinkSync(destFilePath);
+    console.info(
+      `${exports.txtUtils('yellow')}Removed the old file/symlink:\n `,
+      `${exports.txtUtils('reset')}${srcFilePath}`
+    );
   }
 
   fs.symlink(srcFilePath, destFilePath, err => {
     if (err) {
       console.error(err);
       process.exit(err.errno);
-    }
+    } else
+      console.info(
+        `${exports.txtUtils('green')}Created a new symlink to ${destFileName}:\n `,
+        `${exports.txtUtils('reset')}${destFilePath}`
+      );
   });
-  console.info(`${exports.getColour('FgGreen')}Created a symlink to ${destFileName}`);
 };
