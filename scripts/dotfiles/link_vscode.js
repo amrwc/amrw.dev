3e451334e44df66df3d52e2e8403e7057ae46a23
amrwc <42574496+amrwc@users.noreply.github.com>
2019-09-13 20:01:49 +0200

    Refactor 'link_vscode.js'
    - refactor 'link_vscode.js' and move the logic finding the repo-
      sitory's root directory to a reusable function,
    - add a coloured error message to the traversing operation and
      null result in case the loop doesn't find the destination dir.

--- scripts/dotfiles/link_vscode.js
+++ scripts/dotfiles/link_vscode.js
@@ -12,24 +12,17 @@
 
 const os = require('os');
 const path = require('path');
-const { linkDotfile } = require('./link_utils');
+const { findRootDir, linkDotfile } = require('./link_utils');
 
 // VS Code preferences location
 const DESTINATION_DIR_PATH = `${os.homedir()}/Library/Application Support/Code/User`;
-// Name of the root of the repository
+// Name of the root directory of the repository
 const ROOT_DIR_NAME = 'amrwc.github.io';
 
 // Traverse the directories upwards to find the repository's root directory.
-let perhapsRootDir = __dirname;
-while (path.basename(perhapsRootDir) !== ROOT_DIR_NAME) {
-  perhapsRootDir = path.resolve(`${perhapsRootDir}/../`);
-  if (perhapsRootDir === '/') {
-    console.error(`Error: Couldn't locate the root '${ROOT_DIR_NAME}'.`);
-    process.exit(1);
-  }
-}
+const rootDirPath = findRootDir(ROOT_DIR_NAME, __dirname);
 // Absolute path to the source dotfiles
-const SOURCE_DIR_PATH = path.resolve(`${perhapsRootDir}/dotfiles/vscode`);
+const SOURCE_DIR_PATH = path.resolve(`${rootDirPath}/dotfiles/vscode`);
 
 linkDotfile('keybindings.json', 'keybindings.json', SOURCE_DIR_PATH, DESTINATION_DIR_PATH);
 linkDotfile('settings.json', 'settings.json', SOURCE_DIR_PATH, DESTINATION_DIR_PATH);
